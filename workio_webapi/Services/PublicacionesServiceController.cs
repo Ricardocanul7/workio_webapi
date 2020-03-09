50834f15368d3ba1d1193f633fd6577c8c0ff278
Ricardo Canul <ricardocanul7@gmail.com>
2020-03-09 10:22:50 -0600

    Se agrega metodo para consultar trabajo por offset o paginado

---
+++ workio_webapi/Services/PublicacionesServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using workio_webapi.Models;
+
+namespace workio_webapi.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublicacionesServiceController : ControllerBase
+    {
+        // Comienza desde 0 como un arreglo
+        [HttpGet]
+        public IEnumerable<PublicacionTrabajo> GetPublicacionesOffset(int offset)
+        {
+            int range = 10;
+
+            using(var db = new dbworkioContext())
+            {
+                var results = db.PublicacionTrabajo.Skip(offset).Take(range);
+                return results.ToList();
+            }
+        }
+    }
+}
