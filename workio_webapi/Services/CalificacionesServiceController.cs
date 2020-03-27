d3f25326906da731c05976a2b6c296604ae42cc5
Ricardo Canul <ricardocanul7@gmail.com>
2020-03-27 11:53:56 -0600

    Se crean metodos para servicio de calificaciones

---
+++ workio_webapi/Services/CalificacionesServiceController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using workio_webapi.Models;
+
+namespace workio_webapi.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionesServiceController : ControllerBase
+    {
+        private readonly dbworkioContext _context;
+
+        public CalificacionesServiceController(dbworkioContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<Calificaciones>> CalificacionesByIdTrabajador(int id)
+        {
+            var calific = _context.Calificaciones.Where(c => c.TrabajadorId == id).ToList();
+
+            if (calific.Any())
+            {
+                return calific;
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<IEnumerable<Calificaciones>> CalificacionesByIdEmpleador(int id)
+        {
+            var calific = _context.Calificaciones.Where(c => c.EmpleadorId == id).ToList();
+
+            if (calific.Any())
+            {
+                return calific;
+            }
+
+            return NotFound();
+        }
+    }
+}
